56e463ad994dbfc0d0688737bdd552c52bfcc7f5
Graham Fairweather <xotic750@gmail.com>
2019-07-26 00:02:27 +0200

    :bookmark: v3.0.14
    Auto build

--- dist/string-quote-x.js
+++ dist/string-quote-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-24T16:28:33.678Z",
+  "date": "2019-07-25T22:02:22.281Z",
   "describe": "",
   "description": "Wrap a string in double quotes.",
   "file": "string-quote-x.js",
   "hash": "51ac11ea6963b7a0c9a5",
   "license": "MIT",
-  "version": "3.0.13"
+  "version": "3.0.14"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
