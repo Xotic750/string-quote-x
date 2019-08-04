58f14cd16d550b5fbce2519496099fff95ec1fff
Graham Fairweather <xotic750@gmail.com>
2019-08-04 17:02:51 +0200

    Auto build

--- dist/string-quote-x.js
+++ dist/string-quote-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-31T22:25:19.836Z",
+  "date": "2019-08-04T15:02:45.338Z",
   "describe": "",
   "description": "Wrap a string in double quotes.",
   "file": "string-quote-x.js",
